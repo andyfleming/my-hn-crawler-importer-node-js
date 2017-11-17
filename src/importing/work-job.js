763fa710c181e52fe762dd9c08d97bc591f751aa
Andy Fleming <andy@andyfleming.com>
2017-11-17 00:35:30 -0800

    returning function to partially apply function with dependency

--- src/importing/work-job.js
+++ src/importing/work-job.js
@@ -1,13 +1,15 @@
 const importStory = require('./import-story')
 const importComment = require('./import-comment')
 
-module.exports = function workJob(job, addJob) {
-    switch (job.type) {
-        case 'story':
-            return importStory(job.data.id, true, addJob)
-        case 'comment':
-            return importComment(job.data.id)
-        default:
-            throw new Error('Invalid job type: ' + job.type)
+module.exports = (conn) => {
+    return function workJob(job, addJob) {
+        switch (job.type) {
+            case 'import-story':
+                return importStory(conn, addJob, job.data.id)
+            case 'import-comment':
+                return importComment(conn, addJob, job.data.id)
+            default:
+                throw new Error('Invalid job type: ' + job.type)
+        }
     }
 }
