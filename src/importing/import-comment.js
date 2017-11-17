c58c966c250f780c00656ecf08e024d189ba8030
Andy Fleming <andy@andyfleming.com>
2017-11-17 13:04:35 -0800

    Implementing story and comment inserts to DB
    (and some additional error checking/handling)

--- src/importing/import-comment.js
+++ src/importing/import-comment.js
@@ -2,11 +2,40 @@ const api = require('../crawling/api')
 
 module.exports = async function importComment(conn, addJob, id) {
     console.log('Importing comment id: ' + id)
-    const comment = (await api.get(`/item/${id}.json`)).data
 
-    if (comment.kids && comment.kids.length > 0) {
-        comment.kids.forEach(childId => {
-            addJob({type: 'import-comment', data: {id: childId}})
-        })
+    try {
+        const comment = (await api.get(`/item/${id}.json`)).data
+
+        // If the comment is deleted, don't continue
+        if (comment.deleted) {
+            return
+        }
+
+        if (!comment || !comment.id || !comment.parent || !comment.text || !comment.by || !comment.time) {
+            throw new Error('missing expected keys for comment id: ' + id)
+        }
+
+        const values = [
+            comment.id,
+            comment.parent,
+            comment.text,
+            comment.by,
+            comment.time
+        ]
+
+        await conn.execute("INSERT IGNORE INTO comments (hn_id, parent_hn_id, text, username, time) VALUES (?, ?, ?, ?, FROM_UNIXTIME(?))", values)
+
+        if (comment.kids && comment.kids.length > 0) {
+            comment.kids.forEach(childId => {
+                addJob({type: 'import-comment', data: {id: childId}})
+            })
+        }
+    } catch (err) {
+        console.log('Error importing comment')
+        console.log(err)
+        if (typeof comment === typeof {}) {
+            console.log('comment data from API response: ' + JSON.stringify(comment))
+        }
     }
+
 }
--- src/importing/import-story.js
+++ src/importing/import-story.js
@@ -2,13 +2,56 @@ const api = require('../crawling/api')
 
 module.exports = async function importStory(conn, addJob, id) {
     console.log('Importing story id: ' + id)
-    const story = (await api.get(`/item/${id}.json`)).data
-    
-    console.log('Mock importing story: ' + story.title)
 
-    if (story.kids && story.kids.length > 0) {
-        story.kids.forEach(childId => {
-            addJob({type: 'import-comment', data: {id: childId}})
-        })
+    try {
+        const story = (await api.get(`/item/${id}.json`)).data
+        
+        // Example Response:
+        /*
+        {
+            "by": "dhouston",
+            "descendants": 71,
+            "id": 8863,
+            "kids": [
+                8952,
+                8876
+            ],
+            "score": 111,
+            "time": 1175714200,
+            "title": "My YC app: Dropbox - Throw away your USB drive",
+            "type": "story",
+            "url": "http://www.getdropbox.com/u/2/screencast.html"
+        }
+        */
+
+        if (!story || !story.id || !story.title || !story.score || !story.by || !story.time || !story.type) {
+            throw new Error('missing expected keys for story id: ' + id + ' - value: ' + JSON.stringify(story))
+        }
+
+        const values = [
+            story.id,
+            story.title,            
+            story.score,
+            story.by,
+            story.time,
+            story.type,
+            story.url || ''
+        ]
+
+        await conn.execute("INSERT IGNORE INTO stories (hn_id, title, score, username, time, type, url) VALUES (?, ?, ?, ?, FROM_UNIXTIME(?), ?, ?)", values)
+
+        if (story.kids && story.kids.length > 0) {
+            //console.log('adding ' + story.kids.length + ' child comment jobs')            
+            story.kids.forEach(childId => {
+                addJob({type: 'import-comment', data: {id: childId}})
+            })
+        }
+
+    } catch (err) {
+        console.log('Error importing story')
+        console.log(err)
+        if (typeof story === typeof {}) {
+            console.log('story data from API response: ' + JSON.stringify(story))
+        }
     }
 }
