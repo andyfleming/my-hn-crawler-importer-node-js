8139d5c5129eebead6429191c99f001234960e39
Andy Fleming <andy@andyfleming.com>
2017-11-17 13:02:24 -0800

    Implementing idempotent table creation

--- src/db/create-tables.js
+++ src/db/create-tables.js
@@ -1,6 +1,27 @@
 module.exports = async function createDb(conn) {
    
-    const [rows, fields] = await conn.query('SELECT 1')
+    await conn.query(`CREATE TABLE IF NOT EXISTS stories (
+        id int(11) unsigned NOT NULL AUTO_INCREMENT,
+        hn_id int(11) DEFAULT NULL,
+        title text,
+        score int(11) DEFAULT NULL,
+        username varchar(255) DEFAULT NULL,
+        time timestamp NULL DEFAULT NULL,
+        type varchar(100) DEFAULT NULL,
+        url text,
+        PRIMARY KEY (id),
+        UNIQUE KEY hn_id (hn_id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4`)
 
-    console.log(rows)
+      await conn.query(`CREATE TABLE IF NOT EXISTS comments (
+        id int(11) unsigned NOT NULL AUTO_INCREMENT,
+        hn_id int(11) DEFAULT NULL,
+        parent_hn_id int(11) DEFAULT NULL,
+        text text,
+        username varchar(255) DEFAULT NULL,
+        time timestamp NULL DEFAULT NULL,
+        PRIMARY KEY (id),
+        UNIQUE KEY hn_id (hn_id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4`)
+
 }
